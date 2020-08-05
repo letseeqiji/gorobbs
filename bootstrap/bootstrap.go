772857a3abf597134b624e9efdc29d00a29ed62a
qiji <z18553476983@gmail.com>
2020-08-05 17:06:53 +0800

    引入trie树

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -3,9 +3,11 @@ package bootstrap
 import (
 	emailtool "gorobbs/tools/email"
 	searchtool "gorobbs/tools/search"
+	seneitivetool "gorobbs/tools/sensitivewall"
 )
 
 func SetUp() {
 	go emailtool.SendQueueEmail()
 	go searchtool.SearchInit()
+	go seneitivetool.Trieinit()
 }
