76c30d3ab836a342b98a6640f65a5ce5ca362599
letseeqiji <51554380+letseeqiji@users.noreply.github.com>
2021-04-26 17:35:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	//"github.com/fvbock/endless"
 	"gorobbs/bootstrap"
 	"gorobbs/package/setting"
 	router "gorobbs/router/v1"
@@ -26,19 +25,4 @@ func main() {
 	if err := s.ListenAndServe(); err != nil {
 		panic(err.Error())
 	}
-
-	/*endless.DefaultReadTimeOut = setting.ServerSetting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.ServerSetting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
-
-	server := endless.NewServer(endPoint, router.InitRouter())
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Server err: %v", err)
-	}*/
 }
